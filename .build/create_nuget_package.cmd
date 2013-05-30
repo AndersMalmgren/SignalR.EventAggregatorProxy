a9b99d5b97dd75734fcac9cc3bdd13e5f6cba061
AndersMalmgren <anders.malmgren@agero.se>
2013-05-30 10:14:09 +0200

    Added possibility to add release notes from command line when building nuget package

--- .build/create_nuget_package.cmd
+++ .build/create_nuget_package.cmd
@@ -4,6 +4,6 @@ rem go to current folder
 cd %~dp0
 
 msbuild write_VersionInfo.proj
-msbuild build_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
+msbuild build_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
 
 msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
