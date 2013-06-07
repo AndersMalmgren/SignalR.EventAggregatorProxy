00c4b3e50bc3836f6c14bcb679eb581de1d32c34
AndersMalmgren <anders.malmgren@agero.se>
2013-06-07 10:37:56 +0200

    Added possibility to use static typed constraints

--- SignalR.EventAggregatorProxy.Constraint/EventConstraintHandler.cs
+++ SignalR.EventAggregatorProxy.Constraint/EventConstraintHandler.cs
@@ -1,11 +1,25 @@
-﻿namespace SignalR.EventAggregatorProxy.Constraint
+﻿using Newtonsoft.Json.Linq;
+
+namespace SignalR.EventAggregatorProxy.Constraint
 {
-    public abstract class EventConstraintHandler<T> : IEventConstraintHandler<T>
+    public abstract class EventConstraintHandler<TEvent> : IEventConstraintHandler<TEvent>
     {
         public bool Allow(object message, string username, dynamic constraint)
         {
-            return Allow((T)message, username, constraint);
+            return Allow((TEvent)message, username, constraint);
         }
-        public abstract bool Allow(T message, string username, dynamic constraint);
+        public abstract bool Allow(TEvent message, string username, dynamic constraint);
+    }
+
+    public abstract class EventConstraintHandler<TEvent, TConstraint> : EventConstraintHandler<TEvent> where TConstraint : class
+    {
+        public override bool Allow(TEvent message, string username, dynamic constraint)
+        {
+            var jObject = constraint as JObject;
+            var staticTypedConstraint = jObject != null ? jObject.ToObject<TConstraint>() : null;
+
+            return Allow(message, username, staticTypedConstraint);
+        }
+        public abstract bool Allow(TEvent message, string username, TConstraint constraint);
     }
 }
