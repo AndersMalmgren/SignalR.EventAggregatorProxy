091dc86032e801a8a95c9c1afb6c29e949bceb9f
AndersMalmgren <anders.malmgren@agero.se>
2013-06-19 16:18:59 +0200

    Updated package versions

--- .build/create_nuget_package.cmd
+++ .build/create_nuget_package.cmd
@@ -3,6 +3,8 @@ set PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319.
 rem go to current folder
 cd %~dp0
 
-msbuild build_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
+msbuild build_server_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
+
+msbuild build_dot_net_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~2"
 
 msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
