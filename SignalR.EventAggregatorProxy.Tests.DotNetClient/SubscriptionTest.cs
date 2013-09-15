9583d3b5e4687fb572776bc770e9b75658d6ae7c
Anders Malmgren <anders.malmgren@agero.se>
2013-09-15 20:21:13 +0200

    Added more Subscription tests to .Net Client

--- SignalR.EventAggregatorProxy.Tests.DotNetClient/SubscriptionTest.cs
+++ SignalR.EventAggregatorProxy.Tests.DotNetClient/SubscriptionTest.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNet.SignalR.Client.Hubs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhino.Mocks;
 using SignalR.EventAggregatorProxy.Client.Bootstrap.Factories;
+using SignalR.EventAggregatorProxy.Client.Constraint;
 using SignalR.EventAggregatorProxy.Client.EventAggregation;
 
 namespace SignalR.EventAggregatorProxy.Tests.DotNetClient
@@ -22,12 +23,26 @@ namespace SignalR.EventAggregatorProxy.Tests.DotNetClient
         
     }
 
-    [TestClass]
-    public class When_subscribing_to_multiple_events_of_same_type : DotNetClientTest
+    public class GenericEvent<T> : Event
+    {
+        
+    }
+
+    public class StandardEventConstraint
+    {
+        public int Id { get; set; }
+    }
+
+    public abstract class SubscriptionTest<TEvent> : DotNetClientTest where TEvent : class
     {
         private int subscriptionCount = 0;
 
-        [TestInitialize]
+        protected virtual IEnumerable<IConstraintInfo> GetConstraintInfos()
+        {
+            return new List<IConstraintInfo>();
+        }
+            
+            [TestInitialize]
         public void Context()
         {
             var reset = new AutoResetEvent(false);
@@ -37,28 +52,30 @@ namespace SignalR.EventAggregatorProxy.Tests.DotNetClient
                 .Return(new Subscription());
             WhenCalling<IHubProxy>(x => x.Invoke(Arg<string>.Is.Equal("subscribe"), Arg<object[]>.Is.Anything))
                 .Callback<string, object[]>((m, a) =>
-                    {
-                        subscriptionCount += (a[0] as IEnumerable<dynamic>).Count();
-                        reset.Set();
-                        return true;
-                    })
+                {
+                    subscriptionCount += (a[0] as IEnumerable<dynamic>).Count();
+                    reset.Set();
+                    return true;
+                })
                 .Return(null);
 
             Mock<IHubProxyFactory>();
-            
+
             WhenCalling<IHubProxyFactory>(x => x.Create(Arg<string>.Is.Anything, Arg<Action<IHubConnection>>.Is.Anything, Arg<Action<IHubProxy>>.Is.Anything))
                 .Callback<string, Action<IHubConnection>, Action<IHubProxy>>((u, c, started) =>
-                    {
-                        started(proxy);
-                        return true;
-                    })
+                {
+                    started(proxy);
+                    return true;
+                })
                 .Return(proxy);
+
+            var constraint = GetConstraintInfos();
 
             var eventAggregator = new EventAggregator<Event>()
                 .Init("foo");
 
             for (int i = 0; i < 2; i++)
-                eventAggregator.Subscribe(Mock<IHandle<StandardEvent>>());
+                eventAggregator.Subscribe(Mock<IHandle<TEvent>>(), constraint);
 
             reset.WaitOne(50);
         }
@@ -67,6 +84,27 @@ namespace SignalR.EventAggregatorProxy.Tests.DotNetClient
         public void It_should_only_call_server_side_subscribe_once()
         {
             Assert.AreEqual(1, subscriptionCount);
+        }
+    }
+
+    [TestClass]
+    public class When_subscribing_to_multiple_events_of_same_type : SubscriptionTest<StandardEvent>
+    {
+
+    }
+
+    [TestClass]
+    public class When_subscribing_to_multiple_generic_events_of_same_type : SubscriptionTest<GenericEvent<string>>
+    {
+
+    }
+
+    [TestClass]
+    public class When_subscribing_to_multiple_constrained_events_of_same_type : SubscriptionTest<GenericEvent<string>>
+    {
+        protected override IEnumerable<IConstraintInfo> GetConstraintInfos()
+        {
+            return new[] {new ConstraintInfo<StandardEvent, StandardEventConstraint>(new StandardEventConstraint {Id = 1})};
         }
     }
 }
