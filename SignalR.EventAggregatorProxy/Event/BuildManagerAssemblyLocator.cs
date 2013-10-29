fe269ccc4bf8b4e8284de23ff52c975524ad449e
AndersMalmgren <anders.malmgren@agero.se>
2013-10-29 17:08:34 +0100

    SignalR 2.0 Owin pipeline

--- SignalR.EventAggregatorProxy/Event/BuildManagerAssemblyLocator.cs
+++ SignalR.EventAggregatorProxy/Event/BuildManagerAssemblyLocator.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Web.Compilation;
-using Microsoft.AspNet.SignalR.SystemWeb.Infrastructure;
 
 namespace SignalR.EventAggregatorProxy.Event
 {
---
+++ SignalR.EventAggregatorProxy/Owin/AppBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using SignalR.EventAggregatorProxy.Boostrap;
+
+namespace SignalR.EventAggregatorProxy.Owin
+{
+    public static class AppBuilderExtensions
+    {
+        public static void MapEventProxy<TEvent>(this IAppBuilder app)
+        {
+            Bootstrapper.Init<TEvent>();
+            app.Map("/eventAggregation/events", subApp => subApp.Use<EventScriptMiddleware<TEvent>>());
+        }
+    }
+}
