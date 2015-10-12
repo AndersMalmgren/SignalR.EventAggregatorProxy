2e0c710bf42d63ef2f5b8291f5466f2524474ab6
AndersMalmgren <anders.malmgren@agero.se>
2015-10-12 14:24:42 +0200

    Angular client

--- .build/create_nuget_package.cmd
+++ .build/create_nuget_package.cmd
@@ -3,8 +3,10 @@ set PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319.
 rem go to current folder
 cd %~dp0
 
-msbuild build_server_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
+rem msbuild build_server_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
 
-msbuild build_dot_net_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~2"
+rem msbuild build_dot_net_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~2"
 
-msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
+msbuild build_angular_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~3"
+
+rem msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
---
+++ .build/readme/SignalR.EventAggregatorProxy.Client.Angular/readme.txt
@@ -0,0 +1,2 @@
+Please see https://github.com/AndersMalmgren/SignalR.EventAggregatorProxy/wiki/Angular-Client for more information on using this client with the SignalR EventAggregator proxy server library.
+
