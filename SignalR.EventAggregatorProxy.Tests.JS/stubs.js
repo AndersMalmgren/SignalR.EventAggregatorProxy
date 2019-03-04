6af249eb6c61eb478c6e0b662643e2c1c68cc57a
Anders Malmgren <anders.malmgren@insclear.se>
2019-03-04 15:44:29 +0100

    Fixed failed javascript client tests after reconnect refactor

--- SignalR.EventAggregatorProxy.Tests.JS/stubs.js
+++ SignalR.EventAggregatorProxy.Tests.JS/stubs.js
@@ -1,20 +1,34 @@
-﻿(window.stubHub = function() {
+﻿(window.stubHub = function () {
 	window.signalR = {};
     window.signalR.HubConnectionBuilder = function() {
 
     };
 
+    var promiseStub = function () {
+
+    };
+
+    promiseStub.prototype = {
+        then: function(callback) {
+            callback();
+            return this;
+        },
+        catch: function(callback) {
+            return this;
+        }
+    };
+
     window.signalR.HubConnectionBuilder.prototype =
     {
         withUrl: function() { return this },
-        build: function() {
+        build: function () {
             return window.currentHub = window.currentHub ||
             {
                 invoke: function() {
                     throw "Invoke called without test setting up assert on this";
                 },
                 start: function() {
-                    return { then: function(callback) { callback(); } };
+                    return new promiseStub();
                 },
                 on: function() {},
                 onclose: function(callback) {
