43acb13bf6aca10df9d48e28523edeabd1abc2b0
Anders Malmgren <anders.malmgren@outlook.com>
2019-03-13 22:03:08 +0100

    Fixed build errors fro dotnet cli

--- .build/create_nuget_package.cmd
+++ .build/create_nuget_package.cmd
@@ -3,20 +3,20 @@ set PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319.
 rem go to current folder
 cd %~dp0
 
-rem dotnet msbuild build_server_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
+dotnet msbuild build_server_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~1"
 
-rem dotnet msbuild build_server_web_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~2"
+dotnet msbuild build_server_web_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~2"
 
-rem dotnet msbuild build_dot_net_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~3"
+dotnet msbuild build_dot_net_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~3"
 
-rem msbuild build_angular_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~4"
+msbuild build_angular_client_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~4"
 
-rem dotnet msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
+dotnet msbuild build_constraint_output.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver
 
 
-rem msbuild build_js_client.proj /property:NugetFolder=..\NugetOutput 
+msbuild build_js_client.proj /property:NugetFolder=..\NugetOutput 
 
-rem msbuild build_vue_client.proj /property:NugetFolder=..\NugetOutput 
+msbuild build_vue_client.proj /property:NugetFolder=..\NugetOutput 
 
 dotnet msbuild build_dotnet_cli.proj /property:BuildDir=..\Output /property:NugetFolder=..\NugetOutput /property:DeliverFolder=..\Deliver /property:ReleaseNotes="%~5"
 
