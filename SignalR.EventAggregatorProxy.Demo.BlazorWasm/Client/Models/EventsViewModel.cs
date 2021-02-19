6c933a654c03398b908fac4d1fad8720d05705de
Anders Malmgren <anders.malmgren@outlook.com>
2021-02-19 20:39:40 +0100

    Removed unused code

--- SignalR.EventAggregatorProxy.Demo.BlazorWasm/Client/Models/EventsViewModel.cs
+++ SignalR.EventAggregatorProxy.Demo.BlazorWasm/Client/Models/EventsViewModel.cs
@@ -6,40 +6,40 @@ using SignalR.EventAggregatorProxy.Demo.Contracts.Events;
 
 namespace SignalR.EventAggregatorProxy.Demo.BlazorWasm.Client.Models
 {
-    public class EventsViewModel : IHandle<StandardEvent>, IHandle<GenericEvent<string>>, IHandle<ConstrainedEvent>, IHandle<ClientSideEvent>
-    {
-        public EventsViewModel(IProxyEventAggregator eventAggregator)
+        public class EventsViewModel : IHandle<StandardEvent>, IHandle<GenericEvent<string>>, IHandle<ConstrainedEvent>, IHandle<ClientSideEvent>
         {
-            Events = new List<IMessageEvent<string>>();
-            eventAggregator.Subscribe(this, builder => builder.For<ConstrainedEvent>().Add(new ConstrainedEventConstraint { Message = "HelloWorld" }));
+            public EventsViewModel(IProxyEventAggregator eventAggregator)
+            {
+                Events = new List<IMessageEvent<string>>();
+                eventAggregator.Subscribe(this, builder => builder.For<ConstrainedEvent>().Add(new ConstrainedEventConstraint { Message = "HelloWorld" }));
+            }
+
+
+            public List<IMessageEvent<string>> Events { get; }
+
+            public void Handle(StandardEvent message)
+            {
+                Add(message);
+            }
+
+            public void Handle(GenericEvent<string> message)
+            {
+                Add(message);
+            }
+
+            public void Handle(ConstrainedEvent message)
+            {
+                Add(message);
+            }
+
+            public void Handle(ClientSideEvent message)
+            {
+                Add(message);
+            }
+
+            private void Add(IMessageEvent<string> message)
+            {
+                Events.Add(message);
+            }
         }
-
-
-        public List<IMessageEvent<string>> Events { get; }
-
-        public void Handle(StandardEvent message)
-        {
-            Add(message);
-        }
-
-        public void Handle(GenericEvent<string> message)
-        {
-            Add(message);
-        }
-
-        public void Handle(ConstrainedEvent message)
-        {
-            Add(message);
-        }
-
-        public void Handle(ClientSideEvent message)
-        {
-            Add(message);
-        }
-
-        private void Add(IMessageEvent<string> message)
-        {
-            Events.Add(message);
-        }
-    }
 }
